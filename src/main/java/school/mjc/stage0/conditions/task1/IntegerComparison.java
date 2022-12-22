6d3992b29702779be8ede0549f9786925be5b1e1
Dmitry Golub <dmgolub@mail.ru>
2022-12-23 01:08:12 +0400

    Task completed.

--- src/main/java/school/mjc/stage0/conditions/task1/IntegerComparison.java
+++ src/main/java/school/mjc/stage0/conditions/task1/IntegerComparison.java
@@ -1,10 +1,11 @@
 package school.mjc.stage0.conditions.task1;
 
 public class IntegerComparison {
+
     public static void main(String[] args) {
         int x = 50;
         int y = 50;
-        __ (x __ y) {
+        if (x == y) {
             System.out.println("Hello, World!");
         }
     }
